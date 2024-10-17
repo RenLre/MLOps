a3f9965790ac7e82eac766d4f664dbf426b14bee
RenLre <yar.erol.22@heilbronn.dhbw.de>
2024-10-17 20:41:38 +0200

    training in form of functions (due to dag import error)

--- model_training/model_training_test.py
+++ model_training/model_training_test.py
@@ -1,50 +1,48 @@
+import random
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.datasets import mnist
-import random
 
+def load_and_preprocess_data():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.reshape((x_train.shape[0], 28 * 28)).astype('float32') / 255
+    x_test = x_test.reshape((x_test.shape[0], 28 * 28)).astype('float32') / 255
+    y_train = tf.keras.utils.to_categorical(y_train, 10)
+    y_test = tf.keras.utils.to_categorical(y_test, 10)
+    return x_train, y_train, x_test, y_test
 
-# Load MNIST data
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-# Preprocess the data
-x_train = x_train.reshape((x_train.shape[0], 28 * 28)).astype('float32') / 255
-x_test = x_test.reshape((x_test.shape[0], 28 * 28)).astype('float32') / 255
-# One-hot encode the labels
-y_train = tf.keras.utils.to_categorical(y_train, 10)
-y_test = tf.keras.utils.to_categorical(y_test, 10)
-# Function to create a model with random hyperparameters
 def create_model(num_layers, units_per_layer, activation_function, dropout_rate):
     model = models.Sequential()
     model.add(layers.InputLayer(input_shape=(28 * 28,)))
-    
-    # Adding hidden layers
     for _ in range(num_layers):
         model.add(layers.Dense(units_per_layer, activation=activation_function))
-        model.add(layers.Dropout(dropout_rate))  # Optional dropout layer
-    
-    model.add(layers.Dense(10, activation='softmax'))  # Output layer
+        model.add(layers.Dropout(dropout_rate))
+    model.add(layers.Dense(10, activation='softmax'))
     return model
-    # Generate random hyperparameters
-num_layers = random.randint(1, 5)  # Random number of hidden layers between 1 and 5
-units_per_layer = random.choice([32, 64, 128, 256])  # Random units in hidden layers
-activation_function = random.choice(['relu', 'sigmoid', 'tanh'])  # Random activation function
-dropout_rate = random.uniform(0.0, 0.5)  # Random dropout rate between 0.0 and 0.5
 
-# Create the model
-model = create_model(num_layers, units_per_layer, activation_function, dropout_rate)
-# Create the model
-model = create_model(num_layers, units_per_layer, activation_function, dropout_rate)
+def generate_hyperparameters():
+    return {
+        'num_layers': random.randint(1, 5),
+        'units_per_layer': random.choice([32, 64, 128, 256]),
+        'activation_function': random.choice(['relu', 'sigmoid', 'tanh']),
+        'dropout_rate': random.uniform(0.0, 0.5)
+    }
 
-# Compile the model
-model.compile(optimizer='adam', 
-              loss='categorical_crossentropy', 
-              metrics=['accuracy'])
+def train_and_evaluate_model():
+    x_train, y_train, x_test, y_test = load_and_preprocess_data()
+    hyperparameters = generate_hyperparameters()
+    model = create_model(**hyperparameters)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
+    test_loss, test_acc = model.evaluate(x_test, y_test)
+    print(f'\nTest accuracy: {test_acc:.4f}')
+    return test_acc
 
-# Train the model
-model.fit(x_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
+# This function will be called by your Airflow task
+def run_model_training():
+    return train_and_evaluate_model()
 
-# Evaluate the model
-test_loss, test_acc = model.evaluate(x_test, y_test)
-
-print(f'\nTest accuracy: {test_acc:.4f}')
+# Only execute if this script is run directly (not imported)
+if __name__ == "__main__":
+    run_model_training()
